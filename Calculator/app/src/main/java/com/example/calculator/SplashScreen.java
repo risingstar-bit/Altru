dd2e8f9b96c47e118f813a6e399fa1d93a2d65e8
Ashish Kumar Saha <85429920+risingstar-bit@users.noreply.github.com>
2023-02-05 13:47:58 +0530

    I added calculator APP

---
+++ Calculator/app/src/main/java/com/example/calculator/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        Thread thread=new Thread(){
+            public void run(){
+                try {
+                    sleep(2000);
+                    Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        };thread.start();
+    }
+}
